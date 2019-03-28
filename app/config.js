12738e3be0bfecd34efccb41f11347b87fa469e8
wuhuihang908 <125740601@qq.com>
2019-03-28 21:44:38 +0800

    add auth

--- app/config.js
+++ app/config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  dbs: 'mongodb://127.0.0.1:27017/test',
+  // dbs: 'mongodb://127.0.0.1:27017/test',
+  dbs: 'mongodb://huihui:1106062107hH@127.0.0.1:27017/test',
   tokenSecret: 'test'
   // ,redis:{
   //   get host(){
