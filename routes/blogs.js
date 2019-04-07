6c959a32c4044277eda95ca88963c477cf566265
wuhuihang908 <125740601@qq.com>
2019-04-07 16:54:05 +0800

    add jenkins

--- routes/blogs.js
+++ routes/blogs.js
@@ -5,6 +5,8 @@ router.get('/outblogs', blog.getBlogs)
 
 router.get('/outblogs/:id', blog.getBlog)
 
+router.get('/h5blogs', blog.getLatestBlogs)
+
 router.get('/blogs', blog.getBlogs)
 
 router.get('/blogs/:id', blog.getBlog)
@@ -16,7 +18,5 @@ router.del('/blogs/:id', blog.deleteBlog)
 router.put('/blogs/:id', blog.updateBlog)
 
 router.get('/dashboards', blog.getDashboard)
-
-router.get('/h5blogs', blog.getLatestBlogs)
 
 module.exports = router
